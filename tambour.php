20a2a3d432d811bb6b03456dd2ac5f71a50c030e
Aceluffy21 <alexis.retour972@gmail.com>
2020-04-28 17:36:17 +0200

    Fin de l'étape 4

--- tambour.php
+++ tambour.php
@@ -17,7 +17,7 @@ catch(Exception $e)
 ?>
 
 <?php
-$reponse =$bdd->query("SELECT nom, icon FROM instrument");
+$reponse =$bdd->query("SELECT nom, icon, audio FROM instrument");
 
 $tableau = array();
 
@@ -30,12 +30,29 @@ $base = json_encode($tableau);
 
 $result = json_decode($base,true);
 
+echo "<br/>";
 $j = count($result);
+
+ echo "<script>
+           
+       function lancer_son(son)
+       {
+          console.log(son);
+          var audio = document.createElement('AUDIO');
+          audio.setAttribute('id','audio');
+          audio.setAttribute('src','./sounds/' + son);
+          
+          audio.play();
+       }
+      </script>";
 
 for($i=0;$i<$j;$i++)
 {
-    echo "<img src='./images/".$result[$i]["icon"].".png' alt='".$result[$i]["icon"]."'/>";
+  
+    echo '<img src="./images/'.$result[$i]['icon'].'.png" alt="'.$result[$i]["icon"].'" onclick="lancer_son(\''.$result[$i]['audio'].'\');" />';
     echo "<p>".$result[$i]["nom"]."</p>";
+
 }
 $reponse->closeCursor();
 ?>
+
